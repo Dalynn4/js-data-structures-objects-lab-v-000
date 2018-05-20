b3e484dc89319fcdb131e6afcb3442c6a107ee84
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 00:55:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return newdriver
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value
   return driver
-})
+}
